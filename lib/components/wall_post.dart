b6cc9828df85ef7b7b490647131802c4c1f01321
Sekki-vi <kenyonj4@gmail.com>
2024-05-30 11:35:27 -0400

    first commit

--- lib/components/wall_post.dart
+++ lib/components/wall_post.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:project2/components/like_button.dart';
+import '../components/like_button.dart';
 
 class WallPost extends StatefulWidget {
   final String message;
